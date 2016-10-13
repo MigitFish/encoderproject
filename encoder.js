938a60f0692b5589ff746ab8faf8676949329e59
Cannon Nielsen <can_man_14@yahoo.com>
2016-10-13 11:27:45 -0700

    new commit
    addded herioglyhics today and soemthing yesterday

--- encoder.js
+++ encoder.js
@@ -1,37 +1,116 @@
+var textArea = document.getElementById('textArea')  ;
+var letters = {
+  65:"a",
+  66:"b",
+  67:"c",
+  68:"d",
+  69:"e",
+  70:"f",
+  71:"g",
+  72:"h",
+  73:"i",
+  74:"j",
+  75:"k",
+  76:"l",
+  77:"m",
+  78:"n",
+  79:"o",
+  80:"p",
+  81:"q",
+  82:"r",
+  83:"s",
+  84:"t",
+  85:"u",
+  86:"v",
+  87:"w",
+  88:"x",
+  89:"y",
+  90:"z",
+  32:" "
+}
+var images1 = {
+  65:"<img src='images/heiroglyphics/a.gif'>",
+  66:"<img src='images/heiroglyphics/b.gif'>",
+  67:"<img src='images/heiroglyphics/c.gif'>",
+  68:"<img src='images/heiroglyphics/d.gif'>",
+  69:"<img src='images/heiroglyphics/e.gif'>",
+  70:"<img src='images/heiroglyphics/f.gif'>",
+  71:"<img src='images/heiroglyphics/g.gif'>",
+  72:"<img src='images/heiroglyphics/h.gif'>",
+  73:"<img src='images/heiroglyphics/i.gif'>",
+  74:"<img src='images/heiroglyphics/j.gif'>",
+  75:"<img src='images/heiroglyphics/k.gif'>",
+  76:"<img src='images/heiroglyphics/l.gif'>",
+  77:"<img src='images/heiroglyphics/m.gif'>",
+  78:"<img src='images/heiroglyphics/n.gif'>",
+  79:"<img src='images/heiroglyphics/o.gif'>",
+  80:"<img src='images/heiroglyphics/p.gif'>",
+  81:"<img src='images/heiroglyphics/q.gif'>",
+  82:"<img src='images/heiroglyphics/r.gif'>",
+  83:"<img src='images/heiroglyphics/s.gif'>",
+  84:"<img src='images/heiroglyphics/t.gif'>",
+  85:"<img src='images/heiroglyphics/u.gif'>",
+  86:"<img src='images/heiroglyphics/v.gif'>",
+  87:"<img src='images/heiroglyphics/w.gif'>",
+  88:"<img src='images/heiroglyphics/x.gif'>",
+  88:"<img src='images/heiroglyphics/y.gif'>",
+  89:"<img src='images/heiroglyphics/w.gif'>",
+  90:"<img src='images/heiroglyphics/z.gif'>",
+  32:" "
+}
+  /*document.getElementById("inputArea").addEventListener("keydown",function(e){
+    document.getElementById("textArea").innerHTML +=  letters[e.keyCode];
+  });*/
 
-//setting it up to repeat right as typing
-document.getElementById("inputArea").addEventListener("keydown",function(){
-  document.getElementById("textArea").innerHTML = "<p>" + document.getElementById("inputText").value + "</p>"
+
+
+
+
+  //var j = e.keyCode + 1;
+  //var example = letters[j];
+  document.getElementById("inputArea").addEventListener("keydown",function(e){
+
+
+    if (e.keyCode  == 8)
+   {
+     textArea.textContent = textArea.textContent.substr(0, textArea.textContent.length - 1);
+   }
+    //document.getElementById("textArea").innerHTML +=  example;
+    else if($("input:checked").val() == "hieroglyphics"){
+      textArea.innerHTML += images1[e.keyCode];
+
+    }
+    else if($("input:checked").val() == "caesarCipher"){
+      console.log("caesarCipher");
+       if(e.keyCode + 1 == 91)
+      {
+        textArea.textContent += "a";
+      }
+      else if (e.keyCode == 32){
+        textArea.textContent += " ";S
+      }
+      //else if(letters[e.keyCode + 1] === undefined)
+      //{}
+      else if (e.keyCode  == 8)
+      {
+        textArea.textContent = textArea.textContent.substr(0, textArea.textContent.length - 1);
+      }
+
+      else {
+        textArea.textContent += letters[e.keyCode + 1];
+      }
+    }
+    else if($("input:checked").val() == "rune") {
+    console.log("rune")
+    }
+    else if($("input:checked").val() == "echo") {
+      console.log("echo")
+
+
+      textArea.textContent += letters[e.keyCode];
+    }
+  });
+$('input[type="radio"]').click(function(){
+   $('input:checked').prop('checked',false);
+   $(this).prop('checked',true);
 });
-
-//setting up all the buttons and their to be functions
-/*function currentChange(x){
-  var x = 0;
-  if(x==0){
-    toEcho();
-  }
-  else if(x==1){
-    toCeaserCipher();
-  }
-  else if(x=2){
-    toRunes();
-  }
-  else if(x==3){
-    toHieroglyphics();
-  }
-}
-
-
-function toEcho() {
-    document.getElementById('id')
-}
-function toCaeserCipher() {
-
-}
-function toHieroglyphics(){
-
-}
-function toRunes(){
-
-}
-*/
